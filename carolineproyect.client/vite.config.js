a40bb8dd7867591a590ed66c0dd5218000f051eb
MatiasMerkier <129700001+MatiasMerkier@users.noreply.github.com>
2024-11-15 12:25:23 -0300

    upload changes

--- carolineproyect.client/vite.config.js
+++ carolineproyect.client/vite.config.js
@@ -32,9 +32,10 @@ if (env.NODE_ENV === 'development' && (!fs.existsSync(certFilePath) || !fs.exist
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5001';
-
+const basePath = '/CarolineProyect/';
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: basePath,
     plugins: [plugin()],
     resolve: {
         alias: {
